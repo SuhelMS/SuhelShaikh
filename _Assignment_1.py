8cbc6ca7b01a313571fc375976cef272bba0acca
SuhelMS <105543773+SuhelMS@users.noreply.github.com>
2022-06-09 21:26:20 +0530

    Add files via upload

---
+++ _Assignment_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+# *
+# 'hello'
+# -87.8
+# -
+# /
+# +
+# 6
+# 
+# Ans: *, 'hello', -, /,+,6
+# 
+# 2. What is the difference between string and variable?
+# 
+# Ans: String is the data type which is the single quote '', or double quote "".
+# Variable is a place or point where the data is saved.
+# 
+# 3. Describe three different data types.
+# 
+# Integer - Is a number, Float - The value will have decimal point in it, String - Its an alpha-numeric data which needs to be placed in Single or Double Quote ('a',"a")
+# 
+# 4. What is an expression made up of? What do all expressions do?
+# Ans : An expression is made up of variables, operators, and method invocations. An Expression is constructed according to the syntax of the language, that evaluates to a single value.
+# 
+# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# Ans : An expression is a line of code that evaluates to a value. e.g. a+4, 7*3 etc. A statement is an instruction that does something. e.g. a = 7, c = 2+6.
+# 
+# 
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# Ans : 23
+# 
+# 7. What should the values of the following two terms be?
+# spam + spamspam
+# spam * 3
+# Ans : NameError: name 'spam' is not defined
+# 
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# Ans : Variable always starts with a name and or with an underscore, if we add _ to the number 100(_100) then it can be used as a Variable.
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+# int() for Integer, Float() for Floating-point and str() for String.
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten'+99+'burritos.'
+# Ans : TypeError: can only concatenate str (not "int") to str
+# 
+# Suhel Shaikh (Pune)
